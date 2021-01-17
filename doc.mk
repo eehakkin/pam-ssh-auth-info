073a4ab2b7d3d7cf4c721a5837fa21cf46331342
Eero Häkkinen <Eero+git@xn--Hkkinen-5wa.fi>
2021-01-17 23:59:51 +0200

    index.md

--- doc.mk
+++ doc.mk
@@ -6,6 +6,9 @@ SED	= sed
 all: all-html
 all-html: pam_ssh_auth_info.html
 
+all: all-md
+all-md: index.md
+
 # all: all-pdf
 all-pdf: pam_ssh_auth_info.pdf
 
@@ -15,6 +18,10 @@ all-ps: pam_ssh_auth_info.ps
 clean: clean-html
 clean-html:
 	$(RM) -- *.html
+
+clean: clean-md
+clean-md:
+	$(RM) -- index.md
 
 clean: clean-pdf
 clean-pdf:
@@ -27,6 +34,16 @@ clean-ps:
 clean: clean-tmp
 clean-tmp:
 	$(RM) -- *.tmp
+
+index.md: pam_ssh_auth_info.html
+	$(SED) \
+		-e '1i<link href="index.css" rel="stylesheet" type="text/css" />' \
+		-e '\|<body>|N' \
+		-e '1,\|<body>|d' \
+		-e '\|</body>|,$$d' \
+		-- pam_ssh_auth_info.html \
+		> '$@.tmp'
+	$(MV) -- '$@.tmp' '$@'
 
 %.html: %.8
 	$(MAN) -Txhtml -- './$*.8' > '$@.tmp'
