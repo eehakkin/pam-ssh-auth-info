9739da1d7023984b47de15b9b0736d0422dc1fae
Eero Häkkinen <Eero+git@xn--Hkkinen-5wa.fi>
2022-08-20 20:21:27 +0300

    Changed license to GPL/LGPL-3+.

--- pam_syslog.h
+++ pam_syslog.h
@@ -2,8 +2,9 @@
  * Copyright © 2021 Eero Häkkinen <Eero+pam-ssh-auth-info@Häkkinen.fi>
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License version 3
- * as published by the Free Software Foundation.
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
